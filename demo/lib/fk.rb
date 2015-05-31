c5c6a2e155898f97a4d135c259b97754cb1b203d
liupeng <79144876@qq.com>
2015-05-31 16:39:38 +0800

    update:puts

--- demo/lib/fk.rb
+++ demo/lib/fk.rb
@@ -7,14 +7,38 @@ class Fk
 		begin
 		 if (@keyword == nil || @keyword.length == 0)
 		 	 @keyword = 'what\'s the fucking day today!'
-		 end	
-		 puts '############---------------start----------------------########'
-		 puts '#####                                                    #####'
- 		 puts '#####                                                    #####'
-         puts '#####                   '<<@keyword<< '                  #####'
-         puts '#####                                                    #####'
- 		 puts '#####                                                    #####'
- 	 	 puts '############----------------end---------------------- ########'
+		 end
+
+		 num = @keyword.length
+		 sContentLine = '#####          '
+		 sContentLine.concat(@keyword)
+		 sContentLine.concat('          #####')
+		 totalNumber = sContentLine.length
+
+		 $i = 0
+		 sRectLine = ''
+		 while $i < totalNumber  do
+  			 sRectLine.concat('#')
+ 			 $i +=1
+		 end
+
+		 sSpaceLine = '#####          '
+		 $i = 0
+		 while $i < num  do
+  			 sSpaceLine.concat(' ')
+ 			 $i +=1
+		 end
+		 sSpaceLine.concat('          #####')
+
+		 puts '  '
+		 puts sRectLine
+		 puts sSpaceLine
+		 puts sSpaceLine
+		 puts sContentLine
+		 puts sSpaceLine
+		 puts sSpaceLine
+		 puts sRectLine
+
  		 rescue Exception
     			puts "Exception here"
 		 end
